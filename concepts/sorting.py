dbebbf889725457b4c4ce9323db5a3f0b52a8680
vic-lee <viclee@nyu.edu>
2019-05-25 22:53:27 -0400

    Add sort testor

--- concepts/sorting.py
+++ concepts/sorting.py
@@ -7,6 +7,39 @@
 '''
 
 
+def test_sort(sort_func):
+    """Decorator that tests if a sorting algorithm is correct"""
+    def wrapper(*args, **kwargs):
+        unsorted_list = args[0]
+        print("unsorted: \t{}".format(unsorted_list))
+
+        sorted_list = sort_func(*args, **kwargs)
+        sort_list_tested = _sort_tester(*args, **kwargs)
+
+        if sorted_list != sort_list_tested:
+            print("ERROR: sorting output incorrect")
+        else:
+            print("sorted: \t{}".format(sorted_list))
+
+        return sorted_list
+
+    return wrapper
+
+
+def _sort_tester(nums: [int]) -> [int]:
+    """Implemented in insertion sort"""
+    LIST_SIZE = len(nums)
+    for cur in range(LIST_SIZE):
+        key = nums[cur]
+        i = cur - 1
+        while i >= 0 and nums[i] > key:
+            nums[i + 1] = nums[i]
+            i -= 1
+        nums[i + 1] = key
+    return nums
+
+
+@test_sort
 def insertion_sort(nums: [int]) -> [int]:
     LIST_SIZE = len(nums)
     for cur in range(LIST_SIZE):
@@ -21,9 +54,7 @@ def insertion_sort(nums: [int]) -> [int]:
 
 def main():
     unsorted_list = [-1, 2, 100, -3, -37, 7, 6, 99]
-    print("unsorted: \t{}".format(unsorted_list))
     sorted_list = insertion_sort(unsorted_list)
-    print("sorted: \t{}".format(sorted_list))
 
 
 if __name__ == '__main__':
