f57246dfd9b8983984efbe09c399d2f3934f88e6
vic-lee <viclee@nyu.edu>
2019-06-26 11:02:27 -0400

    Add bottom-up fibonacci number solution

--- 509_fibonacci_num.py
+++ 509_fibonacci_num.py
@@ -16,6 +16,7 @@ class Solution:
     - Identify whether recursive calls are repetitive calculations
         -> use `memoization` if repetitive
     """
+
     def __init__(self):
         self.memo = {}
 
@@ -30,3 +31,28 @@ class Solution:
             if n - 2 not in self.memo:
                 self.memo[n-2] = self.fib(n-2)
             return self.memo[n-1] + self.memo[n-2]
+
+
+class Solution_alt:
+    """
+    A bottom-up, non-recursive, memoized solution. Computationally, this
+    solution is identical to the one above.
+
+    Result
+    ------
+    - Runtime:      32ms    (91.93%)
+    - Memory:       13Mb    (91.37%)
+    """
+
+    def __init__(self):
+        self.memo = {}
+
+    def fib(self, n: int) -> int:
+        for n in range(n+1):
+            if n == 0:
+                self.memo[n] = 0
+            elif n == 1:
+                self.memo[n] = 1
+            else:
+                self.memo[n] = self.memo[n-1] + self.memo[n-2]
+        return self.memo[n]
