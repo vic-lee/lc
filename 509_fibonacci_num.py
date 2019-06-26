99b77a471efd3d3d6f8825662f51f84206199860
vic-lee <viclee@nyu.edu>
2019-06-26 10:36:23 -0400

    Add fibonacci number calculation

---
+++ 509_fibonacci_num.py
@@ -0,0 +1,27 @@
+"""
+509. Fibonacci Number
+src: https://leetcode.com/problems/fibonacci-number/
+"""
+
+
+class Solution:
+    """
+    Key takeaways
+    -------------
+    - Identify whether recursive calls are repetitive calculations
+        -> use `memoization` if repetitive
+    """
+    def __init__(self):
+        self.memo = {}
+
+    def fib(self, n: int) -> int:
+        if n == 0:
+            return 0
+        elif n == 1:
+            return 1
+        else:
+            if n - 1 not in self.memo:
+                self.memo[n-1] = self.fib(n-1)
+            if n - 2 not in self.memo:
+                self.memo[n-2] = self.fib(n-2)
+            return self.memo[n-1] + self.memo[n-2]
