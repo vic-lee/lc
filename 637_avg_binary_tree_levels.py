b82b8b89e04ebe85052d27358778b973ac48f367
vic-lee <viclee@nyu.edu>
2019-06-25 22:10:26 -0400

    Add BT level problem

---
+++ 637_avg_binary_tree_levels.py
@@ -0,0 +1,67 @@
+"""
+637. Average of Levels in Binary Tree
+src: https://leetcode.com/problems/average-of-levels-in-binary-tree/
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution:
+    """
+    v2: simplify mean()
+    """
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root:
+            return []
+
+        level = [root]
+        ans = []
+        while len(level) > 0:
+
+            next_level = []
+            for node in level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+
+            ans.append(self.mean(level))
+            level = next_level
+
+        return ans
+
+    def mean(self, lst: List[TreeNode]) -> float:
+        return sum(map(lambda n: n.val, lst)) / len(lst)
+
+
+class Solution_v1:
+    def averageOfLevels(self, root: TreeNode) -> List[float]:
+        if not root:
+            return []
+
+        level = [root]
+        ans = []
+        while len(level) > 0:
+
+            next_level = []
+            for node in level:
+                if node.left:
+                    next_level.append(node.left)
+                if node.right:
+                    next_level.append(node.right)
+
+            ans.append(self.mean(level))
+            level = next_level
+
+        return ans
+
+    def mean(self, lst: List[TreeNode]) -> float:
+        sum = 0
+        for node in lst:
+            sum += node.val
+        return sum / len(lst)
